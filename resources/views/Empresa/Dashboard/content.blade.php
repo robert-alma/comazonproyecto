2c17d9fcbeb28bb9ad6664fcfceb9fe17a641623
accreativo <carlos.almanza.cabrera@gmail.com>
2021-09-09 00:18:30 -0500

    Creacion Empresa

---
+++ resources/views/Empresa/Dashboard/content.blade.php
@@ -0,0 +1,57 @@
+<h1>Bienvenido Ejecutivo {{ session('usuario') }} </h1>
+<div class="container-lg">
+    <div class="row">
+        <div class="col-lg-12">
+            <h4>Listado de Solicitudes</h4>
+            <div class="table-responsive">
+                <table id="mytable" class="table table-bordred table-striped" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Cliente</th>
+                            <th>Asunto</th>
+                            <th>Comentario</th>
+                            <th>Presupuesto</th>
+                            <th>Fecha</th>
+                            <th>Crear Cotización</th>
+                            <th>Ver Cliente</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($solicitudes as $solicitud)
+                            <tr>
+                                <td>{{ $solicitud->id }}</td>
+                                <td>{{ $solicitud->cliente_id }}</td>
+                                <td>{{ $solicitud->asunto }}</td>
+                                <td>{{ $solicitud->comentario }}</td>
+                                <td>{{ $solicitud->presupuesto }}</td>
+                                <td>{{ $solicitud->fecha_entrega }}</td>
+                                <td><a href="{{ route('ejecutivo.cotizacion', $solicitud->id) }}"><button
+                                            class="btn btn-primary btn-xs">Crear</button></a></td>
+                                <td><a href="{{ route('ejecutivo.vercliente', $solicitud->id) }}"><button
+                                            class="btn btn-info">Ver </button></a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>Id</th>
+                            <th>Cliente</th>
+                            <th>Asunto</th>
+                            <th>Comentario</th>
+                            <th>Presupuesto</th>
+                            <th>Fecha</th>
+                            <th>Crear Cotización</th>
+                            <th>Ver Cliente</th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    $(document).ready(function() {
+        $('#mytable').DataTable();
+    });
+</script>
--- resources/views/Publico/Templates/header.blade.php
+++ resources/views/Publico/Templates/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
-    <div class="container-fluid justify-content-center">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('bienvenido') }}">Comazon</a>
+        {{-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -20,6 +20,6 @@
                     <a class="nav-link" href="{{ route('vistacontraseña') }}">Recuperar Contraseña</a>
                 </li>
             </ul>
-        </div>
+        </div> --}}
     </div>
 </nav>
